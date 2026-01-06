1780680b272bf4816b2c529fd3f645482f578dc3
keishanylaa <keishanyla@gmail.com>
2026-01-06 18:20:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,4 @@
 import streamlit as st
-import base64
-
-def set_bg_from_local(image_file):
-    with open(image_file, "rb") as img:
-        encoded = base64.b64encode(img.read()).decode()
-
-    st.markdown(
-        f"""
-        <style>
-        .stApp {{
-            background-image: url("data:image/jpg;base64,{encoded}");
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-        }}
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-set_bg_from_local("bg.jpeg")
-
 
 # =========================
 # KONFIGURASI HALAMAN
